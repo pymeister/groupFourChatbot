724d47f4e756f96b37028e4fec8adc56a83d99df
brycemuellerr <brycemuellerr@gmail.com>
2024-11-11 22:11:06 -1000

    Update apps.py

--- chatbot/app/apps.py
+++ chatbot/app/apps.py
@@ -1,8 +1,11 @@
 from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
 
-
+# Define the configuration class for the app
 class AppConfig(AppConfig):
+    # Set the default field type for auto-generated primary keys
     default_auto_field = "django.db.models.BigAutoField"
+    # Define the full Python path to the app
     name = "chatbot.app"
+    # Human-Readable name for the app, wrapped in translation utility for localization
     verbose_name = _("Application")

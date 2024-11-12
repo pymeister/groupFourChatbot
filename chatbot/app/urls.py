6040fc7ad4652098375508fd07c8939f2ac9a133
brycemuellerr <brycemuellerr@gmail.com>
2024-11-11 21:56:44 -1000

    Update urls.py

--- chatbot/app/urls.py
+++ chatbot/app/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 
 from .views import ChatView
 
+# Namespace for this app's URL's
 app_name = "app"
 
+# Define URL Patterns for the app
 urlpatterns = [
+    # URL pattern for the chat page; directs the root URL to the ChatView: Calls ChatView when the base URL is accessed
     path("", ChatView.as_view(), name="chat"),
 ]
